bd70f5395bc13722f35fbdeb77bf40749f433757
Abhinav Mishra <mishrabhinav96@gmail.com>
2018-04-25 00:32:29 -0700

    Add history reducer and saga

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux-immutable';
 
 import appReducer from './containers/App/reducer';
+import settingReducer from './containers/Settings/reducer';
+import historyReducer from './containers/History/reducer';
 
 export default function createReducer (asyncReducers) {
   return combineReducers({
     app: appReducer,
+    settings: settingReducer,
+    history: historyReducer,
     ...asyncReducers
   });
 }
--- src/sagas.js
+++ src/sagas.js
@@ -1,7 +1,9 @@
 import { fork } from 'redux-saga/effects';
 
 import appSaga from './containers/App/sagas';
+import historySaga from './containers/History/sagas';
 
 export default function * rootSaga () {
   yield fork(appSaga);
+  yield fork(historySaga);
 }
