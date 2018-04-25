e82b00d1e71ec5ab5cf03d3e1d8383aadabed29f
Abhinav Mishra <mishrabhinav96@gmail.com>
2018-04-25 00:32:51 -0700

    Implement authentication routing

--- src/nav/index.js
+++ src/nav/index.js
@@ -1,23 +1,70 @@
-import { DrawerNavigator } from 'react-navigation';
+import React from 'react';
+import { TabNavigator, StackNavigator, SwitchNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import App from '../containers/App';
-import About from '../containers/About';
 import History from '../containers/History';
 import Settings from '../containers/Settings';
+import Login from '../containers/Login';
+import AuthLoading from '../containers/AuthLoading';
 
-const routeConfig = {
-  Directions: {
-    screen: App
-  },
+const appRouteConfig = {
   History: {
     screen: History
   },
+  Directions: {
+    screen: App
+  },
   Settings: {
-    screen: Settings
-  },
-  About: {
-    screen: About
-  },
+    screen: Settings,
+    title: 'Settings'
+  }
 };
 
-export default DrawerNavigator(routeConfig);
+const appNavigatorConfig = {
+  initialRouteName: 'Settings',
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ tintColor }) => {
+      const { routeName } = navigation.state;
+      if (routeName === 'History') {
+        return <Icon name='history' size={25} color={tintColor} />;
+      } else if (routeName == 'Directions') {
+        return <Icon2 name='directions-fork' size={25} color={tintColor} />;
+      } else if (routeName == 'Settings') {
+        return <Icon name='settings' size={25} color={tintColor} />;
+      }
+    },
+  }),
+  tabBarOptions: {
+    activeTintColor: '#33a5ff',
+    inactiveTintColor: 'gray',
+  },
+  tabBarPosition: 'bottom',
+  swipeEnabled: false,
+};
+
+const authRouteConfig = {
+  Login: Login
+};
+
+const authNavigatorConfig = {
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false,
+  }
+};
+
+const AppStack = TabNavigator(appRouteConfig, appNavigatorConfig);
+const AuthStack = StackNavigator(authRouteConfig, authNavigatorConfig);
+
+export default SwitchNavigator(
+  {
+    AuthLoading: AuthLoading,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+);
